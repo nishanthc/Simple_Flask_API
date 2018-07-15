f32ce81c259fadf352084cc3d8a7b18c73ed4d57
Nish chandradas <nishanthchandradas@gmail.com>
2018-07-15 12:49:09 +0100

    Added message tests for existing tracks

--- api_tests.py
+++ api_tests.py
@@ -1,5 +1,5 @@
 import api
-from flask_restful import Resource, Api, abort
+import json
 import unittest
 
 
@@ -8,6 +8,8 @@ class FlaskrTestCase(unittest.TestCase):
     def setUp(self):
 
         api.app.testing = True
+        with open('tracks.json') as json_file:
+            self.all_tracks = json.load(json_file)
         self.app = api.app.test_client()
 
 
@@ -28,14 +30,23 @@ class FlaskrTestCase(unittest.TestCase):
 
 
         rv = self.app.get('/track/-1')
-        assert b'{\"message\": \"track -1 doesn\'t exist\"}' in rv.data
+        assert '{\"message\": \"track -1 doesn\'t exist\"}' in rv.data.decode()
 
         client = self.app.get('/track/4444')
-        assert b'{\"message\": \"track 4444 doesn\'t exist\"}' in client.data
+        assert '{\"message\": \"track 4444 doesn\'t exist\"}' in client.data.decode()
 
         client = self.app.get('/track/N0TANINT')
-        assert b'{\"message\": \"track N0TANINT doesn\'t exist\"}' in client.data
+        assert '{"message": "track N0TANINT doesn\'t exist"}' in client.data.decode()
 
+
+        client = self.app.get('/track/5')
+        assert '{"track": [{"id": "5", "title": "Paparazzi", "artist": "Lady GaGa", "duration": "199", "last_play": ' \
+               '"2016-02-23 08:24:37"}]}' in client.data.decode()
+
+
+        client = self.app.get('/track/100')
+        assert '{"track": [{"id": "100", "title": "Addicted To Love", "artist": "Robert Palmer", "duration": "188", ' \
+               '"last_play": "2017-03-14 09:33:16"}]}' in client.data.decode()
 
 
 if __name__ == '__main__':

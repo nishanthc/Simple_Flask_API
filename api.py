cb894a99ed71a058c70110d4a2786436a4faf498
Nish chandradas <nishanthchandradas@gmail.com>
2018-07-15 22:35:11 +0100

    Refactored

--- api.py
+++ api.py
@@ -18,7 +18,6 @@ parser.add_argument('artist')
 parser.add_argument('duration')
 parser.add_argument('last_play')
 
-
 # Load JSON File
 with open('tracks.json') as json_file_r:
     try:
@@ -28,10 +27,11 @@ with open('tracks.json') as json_file_r:
         print("There is an issue with the JSON file. The server could not start.")
         exit()
 
-# Load JSON File
-def CheckTrackExists(track_id,error_redirect=True):
+
+def checkTrackExists(track_id, error_redirect=True):
     """Return True if track does not exist, if error_redirect=true and track doesn't exist then 404.
     """
+
     # Attempt to isolate track from the list of all tracks
     track = (list(filter(lambda track: track['id'] == track_id, all_tracks)))
     if not track and error_redirect:
@@ -40,37 +40,41 @@ def CheckTrackExists(track_id,error_redirect=True):
     if track and not error_redirect:
         return True
 
+
 def write_to_file(data):
     """Write new data to the JSON file
     """
 
-    #Add exception handling!
-    with open('tracks.json','w') as json_file_w:
+    # Add exception handling!
+    with open('tracks.json', 'w') as json_file_w:
         json_file_w.write((json.dumps(data)))
 
-def is_int(variable_name,variable_content):
+
+def is_int(variable_name, variable_content):
     """Checks if a variable is an integer or not
     """
     try:
         val = int(variable_content)
     except ValueError:
-        abort(400, message="{} {} is not an integer".format(variable_name,variable_content))
+        abort(400, message="{} {} is not an integer".format(variable_name, variable_content))
+
 
 class SingleTrack(Resource):
     """Returns details of a single track using the get method
 
     """
+
     def get(self, track_id):
-        CheckTrackExists(track_id)
+        checkTrackExists(track_id)
         track = (list(filter(lambda track: track['id'] == track_id, all_tracks)))
         return {'track': track}
-
 
 
 class Track(Resource):
     """The get method returns a list of all tracks
        The post method can be used to create a new track.
     """
+
     def get(self):
         return {'tracks': all_tracks}
 
@@ -85,46 +89,46 @@ class Track(Resource):
         is_int(variable_name="track_id", variable_content=track_id)
         is_int(variable_name="duration", variable_content=duration)
 
-
-        if CheckTrackExists(track_id,error_redirect=False) == True:
+        if checkTrackExists(track_id, error_redirect=False):
             abort(409, message="track {} already exists".format(track_id))
 
-        all_tracks.append({'id':track_id,
-                           'title':title,
-                           'artist':artist,
-                           'duration':duration,
-                           'last_play':last_play})
+        all_tracks.append({'id': track_id,
+                           'title': title,
+                           'artist': artist,
+                           'duration': duration,
+                           'last_play': last_play})
 
         all_tracks_sorted = sorted(all_tracks, key=lambda key: key['id'])
         write_to_file(all_tracks_sorted)
-        return SingleTrack.get(self,track_id=track_id), 201
+        return SingleTrack.get(self, track_id=track_id), 201
 
 
 class LastPlayed(Resource):
     """Using the get method, this can be used to list the last 100 played tracks.
     """
+
     def get(self):
-        all_tracks_sorted = sorted(all_tracks, key=lambda key: key['last_play'],reverse=True)[:100]
-        return {'tracks': all_tracks_sorted},200
+        all_tracks_sorted = sorted(all_tracks, key=lambda key: key['last_play'], reverse=True)[:100]
+        return {'tracks': all_tracks_sorted}, 200
 
 
 class FilterTracks(Resource):
     """Using the get method with an argument (filter_text) to return all tracks with a similar title as the filter_text
         """
-    def get(self,filter_text):
+
+    def get(self, filter_text):
         filtered_list = []
         for track in all_tracks:
             title = track['title'].lower()
             if filter_text.lower() in title:
                 filtered_list.append(track)
-        return {'tracks': filtered_list},200
+        return {'tracks': filtered_list}, 200
 
 
 class Artists(Resource):
     """With the GET method this will return a list of artists with their total number of tracks and most recently
         played track.
         """
-
 
     def get(self):
         list_of_artists = []
@@ -137,14 +141,13 @@ class Artists(Resource):
             datetimestamp_from_file = track['last_play']
             last_played_of_track_from_file = datetime.strptime(datetimestamp_from_file, '%Y-%m-%d %H:%M:%S')
             artist = track['artist']
-            title =  track['title']
+            title = track['title']
             if artist not in artist_last_played_datetime:
-                #print(artist + " was not in the list of artists, last play: "+ last_played_of_track_from_file.strftime('%Y-%m-%d %H:%M:%S') )
                 artist_last_played_datetime[artist] = last_played_of_track_from_file.strftime('%Y-%m-%d %H:%M:%S')
                 artist_last_played_track_title[artist] = title
 
-
-            if  datetime.strptime(artist_last_played_datetime[artist], '%Y-%m-%d %H:%M:%S') < last_played_of_track_from_file:
+            if datetime.strptime(artist_last_played_datetime[artist],
+                                 '%Y-%m-%d %H:%M:%S') < last_played_of_track_from_file:
                 artist_last_played_datetime[artist] = last_played_of_track_from_file.strftime('%Y-%m-%d %H:%M:%S')
                 artist_last_played_track_title[artist] = title
 
@@ -153,11 +156,10 @@ class Artists(Resource):
 
         for artist in list_of_artists:
             if artist:
-                artists_with_data.append({"artist":artist,"plays":artists_track_count[artist],
-                                          "last_played_track":str(artist_last_played_track_title[artist])})
+                artists_with_data.append({"artist": artist, "plays": artists_track_count[artist],
+                                          "last_played_track": str(artist_last_played_track_title[artist])})
         artists_with_data = artists_with_data
         return {'artists': artists_with_data}, 200
-
 
 
 api.add_resource(Track, '/tracks')
@@ -167,7 +169,4 @@ api.add_resource(LastPlayed, '/last_played')
 api.add_resource(Artists, '/artists')
 
 if __name__ == '__main__':
-
     app.run(debug=True)
-
-
--- api_tests.py
+++ api_tests.py
@@ -4,7 +4,6 @@ import unittest
 
 
 class APITestCase(unittest.TestCase):
-
     def reset_file(self):
         with open('tracks_backup.json') as json_file:
             self.all_tracks = json.load(json_file)
@@ -12,9 +11,7 @@ class APITestCase(unittest.TestCase):
         with open('tracks.json', 'w') as json_file_w:
             json_file_w.write(json.dumps(self.all_tracks))
 
-
     def setUp(self):
-
         api.app.testing = True
         APITestCase.reset_file(self)
         self.app = api.app.test_client()
@@ -22,8 +19,7 @@ class APITestCase(unittest.TestCase):
     def tearDown(self):
         APITestCase.reset_file(self)
 
-
-    def newTrack(self,id,title,artist,duration,last_play):
+    def newTrack(self, id, title, artist, duration, last_play):
         client = self.app.post('/tracks', data=dict(
             id=id,
             title=title,
@@ -33,9 +29,7 @@ class APITestCase(unittest.TestCase):
         ))
         return client
 
-
     def test_getSingleTrackStatus(self):
-
         client = self.app.get('/tracks/4444')
         assert "404 NOT FOUND" == client.status
 
@@ -49,7 +43,6 @@ class APITestCase(unittest.TestCase):
         assert "200 OK" == client.status
 
     def test_getSingleTrackMessage(self):
-
         client = self.app.get('/tracks/-1')
         assert '{\"message\": \"track -1 doesn\'t exist\"}' in client.data.decode()
 
@@ -59,66 +52,51 @@ class APITestCase(unittest.TestCase):
         client = self.app.get('/tracks/N0TANINT')
         assert '{"message": "track N0TANINT doesn\'t exist"}' in client.data.decode()
 
-
         client = self.app.get('/tracks/5')
         assert '{"track": [{"id": "5", "title": "Paparazzi", "artist": "Lady GaGa", "duration": "199", "last_play": ' \
                '"2016-02-23 08:24:37"}]}' in client.data.decode()
-
 
         client = self.app.get('/tracks/100')
         assert '{"track": [{"id": "100", "title": "Addicted To Love", "artist": "Robert Palmer", "duration": "188", ' \
                '"last_play": "2017-03-14 09:33:16"}]}' in client.data.decode()
 
-
-
-
     def test_addNewTrackStatusMessage(self):
-
-
         client = APITestCase.newTrack(self, id=1, title="a title of a song", artist="An artists name", duration=532,
-                                       last_play="2017-03-14 09:33:16")
+                                      last_play="2017-03-14 09:33:16")
         assert "409 CONFLICT" == client.status
         assert '{"message": "track 1 already exists"}' in client.data.decode()
 
         client = APITestCase.newTrack(self, id=900000, title="a title of a song", artist="An artists name",
-                                       duration=532, last_play="2017-03-14 09:33:16")
+                                      duration=532, last_play="2017-03-14 09:33:16")
         assert "201 CREATED" == client.status
 
         assert '{"track": [{"id": "900000", "title": "a title of a song", "artist": "An artists name",' \
                ' "duration": "532", "last_play": "2017-03-14 09:33:16"}]}' in client.data.decode()
 
-
         client = APITestCase.newTrack(self, id=900000, title="a title of a different song",
-                                       artist="An different artists name", duration=22, last_play="2017-03-14 09:33:16")
+                                      artist="An different artists name", duration=22, last_play="2017-03-14 09:33:16")
         assert "409 CONFLICT" == client.status
 
         assert '{"message": "track 900000 already exists"}' in client.data.decode()
 
         client = APITestCase.newTrack(self, id=600000, title="a title of a different song",
-                                       artist="An different artists name", duration=232, last_play="2017-03-14 09:33:16")
+                                      artist="An different artists name", duration=232, last_play="2017-03-14 09:33:16")
         assert "201 CREATED" == client.status
 
         assert '{"track": [{"id": "600000", "title": "a title of a different song",' \
                ' "artist": "An different artists name", "duration": "232",' \
                ' "last_play": "2017-03-14 09:33:16"}]}' in client.data.decode()
 
-
-
-
         client = APITestCase.newTrack(self, id="non numerical input", title="a title of a different song",
-                                       artist="an artists name", duration=232, last_play="2017-03-14 09:33:16")
+                                      artist="an artists name", duration=232, last_play="2017-03-14 09:33:16")
         assert "400 BAD REQUEST" == client.status
         assert '{"message": "track_id non numerical input is not an integer"}' in client.data.decode()
 
-
         client = APITestCase.newTrack(self, id=25352, title="a title of a different song",
-                                       artist="an artists name", duration="a random set of letters",
-                                       last_play="2017-03-14 09:33:16")
+                                      artist="an artists name", duration="a random set of letters",
+                                      last_play="2017-03-14 09:33:16")
         assert "400 BAD REQUEST" == client.status
         assert '{"message": "duration a random set of letters is not an integer"}' in client.data.decode()
-
-
-
 
     def test_LastPlayed(self):
         client = self.app.get('/last_played')
@@ -126,8 +104,8 @@ class APITestCase(unittest.TestCase):
         assert "200 OK" == client.status
 
         client = APITestCase.newTrack(self, id=1234567, title="a recently played song",
-                                       artist="an artists name", duration=232,
-                                       last_play="2020-03-14 10:23:26")
+                                      artist="an artists name", duration=232,
+                                      last_play="2020-03-14 10:23:26")
 
         assert "201 CREATED" == client.status
 
@@ -144,7 +122,7 @@ class APITestCase(unittest.TestCase):
                                       last_play="2010-03-14 10:23:26")
         assert "201 CREATED" == client.status
 
-        client = self.app.get('/tracks/filter_by_name/'+test_title)
+        client = self.app.get('/tracks/filter_by_name/' + test_title)
         filtered_tracks = json.loads(client.data.decode())
 
         closest_match = filtered_tracks['tracks'][0]['title']
@@ -170,8 +148,6 @@ class APITestCase(unittest.TestCase):
                                       last_play="2020-03-14 10:23:26")
         assert "201 CREATED" == client.status
 
-
-
         client = self.app.get('/artists')
         artists = json.loads(client.data.decode())
         artists = artists['artists']
@@ -179,9 +155,6 @@ class APITestCase(unittest.TestCase):
         test_artist = next((artist for artist in artists if artist["artist"] == "an artist with 5 songs"))
         assert str(test_artist['artist']) == "an artist with 5 songs"
         assert str(test_artist['last_played_track']) == "The title with the latest play"
-
-
-
 
 
 if __name__ == '__main__':
